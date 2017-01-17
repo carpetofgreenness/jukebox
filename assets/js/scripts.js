92341e35a9a4519fa8063136ed9f2ea079ef020b
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-01-17 16:02:35 -0500

    list the tracks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,11 +20,13 @@ function Jukebox() {
 			this.isPlaying = true;
 			this.isPaused = false;
 			pauseStatus(this)
+			$('.glyphicon-play').addClass('glyphicon-pause');
 		} else if (this.isLoaded == true) {
 			this.playlist[this.place].pause();
 			this.isPlaying = false;
 			this.isPaused = true;
 			pauseStatus(this)
+			$('.glyphicon-play').removeClass('glyphicon-pause');
 		}
 	}
 
@@ -33,6 +35,20 @@ function Jukebox() {
 		this.isLoaded = false;
 		this.isPlaying = false;
 		this.isPaused = false;
+		pauseStatus(this)
+		document.getElementById("songname").innerText = "";
+		$('.glyphicon-play').removeClass('glyphicon-pause');
+	}
+
+	this.next = function() {
+		this.place++;
+		this.place = this.place%this.playlist.length; //start fromt the beginning
+		this.playlist[this.place].load();
+		this.isLoaded = true;
+		this.playlist[this.place].play();
+		this.isPlaying = true;
+		this.isPaused = false;
+		$('.glyphicon-play').addClass('glyphicon-pause');
 		pauseStatus(this)
 	}
 }
@@ -44,6 +60,16 @@ function pauseStatus(jukeboxName) {
 	} else {
 		document.getElementById("playStatus").innerText = " "
 	}
+}
+
+function upNext(jukeboxName) {
+	this.juke = jukeboxName;
+	this.playlist = juke.playlist;
+	this.htmlstring = ""
+	for (var i=0;i<this.playlist.length;i++) {
+		this.htmlstring += "<p id=\"" + this.playlist[i].name + "\">" + this.playlist[i].name + "</p>";
+	}
+	document.getElementById("upnext").innerHTML = this.htmlstring;
 }
 
 juke = new Jukebox();
@@ -81,15 +107,21 @@ function Song(name, filename, jukeboxName) {
 sunny = new Song("sunny","assets/bensound-sunny.mp3",juke);
 buddy = new Song("buddy","assets/bensound-buddy.mp3",juke);
 uke = new Song("ukulele","assets/bensound-ukulele.mp3",juke);
+sunny1 = new Song("sunny1","assets/bensound-sunny.mp3",juke);
+buddy1 = new Song("buddy1","assets/bensound-buddy.mp3",juke);
+uke1 = new Song("ukulele1","assets/bensound-ukulele.mp3",juke);
 
 //play button
 $(".glyphicon-play").click(function() {
 	juke.playOrPause();
-	$('.glyphicon-play').toggleClass('glyphicon-pause');
 });
 
 //stop button
 $(".glyphicon-stop").click(function() {
 	juke.stop();
-	$('.glyphicon-pause').removeClass('glyphicon-pause');
+});
+
+//next button
+$(".glyphicon-step-forward").click(function() {
+	juke.next();
 });
